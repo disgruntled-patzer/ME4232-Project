d44dd2a863a51885b5bde1b33f37328cd8169e91
Lau Yan Han <sps08.lauyanhan@gmail.com>
2021-01-30 13:04:35 +0800

    Generalise odom storage to 6-DOF

--- src/me4232/src/uav.cpp
+++ src/me4232/src/uav.cpp
@@ -15,6 +15,15 @@ Copyright (C) 2021, Lau Yan Han and Niu Xinyuan, National University of Singapor
 #include <tf2_ros/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
+// Describe Pos and Vel for a 6 DOF model (XYZ and RPY) at a certain timestep.
+// Units: m, m/s, rad, rad/s
+typedef struct {
+	double x,y,z,roll,pit,yaw,vx,vy,vz,v_roll,v_pit,v_yaw;
+	ros::Time current_time, last_time;
+} six_dof;
+
+void compute_odom(six_dof *state);
+
 int main(int argc, char **argv){
     
     // Anon is needed since multiple UAV nodes will be created
@@ -24,41 +33,39 @@ int main(int argc, char **argv){
     ros::Rate loop_rate = 1;
     tf2_ros::TransformBroadcaster odom_broadcaster;
 
-    // xy + angular (th) pos and vel. Units: m/s and rad/s
-    double x = 0.0;
-	double y = 0.0;
-	double th = 0.0;
-	double vx = 0.1;
-	double vy = -0.1;
-	double vth = 0.1;
-
-    ros::Time current_time, last_time;
-	current_time = ros::Time::now();
-	last_time = ros::Time::now();
+	six_dof state;
+	state.x = 0.0;
+	state.y = 0.0;
+	state.z = 0.0;
+	state.roll = 0.0;
+	state.pit = 0.0;
+	state.yaw = 0.0;
+	state.vx = 0.1;
+	state.vy = -0.1;
+	state.vz = 0.0;
+	state.v_roll = 0.0;
+	state.v_pit = 0.0;
+	state.v_yaw = 0.1;
+	state.current_time = ros::Time::now();
+	state.last_time = ros::Time::now();
 
     while (ros::ok()){
 
         // Compute odometry in a typical way given the velocities of the robot
-        double dt = (current_time - last_time).toSec();
-		double delta_x = (vx * cos(th) - vy * sin(th)) * dt;
-		double delta_y = (vx * sin(th) + vy * cos(th)) * dt;
-		double delta_th = vth * dt;
-		x += delta_x;
-		y += delta_y;
-		th += delta_th;
+        compute_odom(&state);
 
         // Since all odometry is 6DOF we'll need a quaternion created from yaw
 		//geometry_msgs::Quaternion odom_quat = tf2_ros::createQuaternionMsgFromYaw(th);
         tf2::Quaternion odom_quat;
-        odom_quat.setRPY(0,0,th);
+        odom_quat.setRPY(state.v_roll,state.v_pit,state.v_yaw);
 
         // Publish the transform over tf
 		geometry_msgs::TransformStamped odom_trans;
-		odom_trans.header.stamp = current_time;
+		odom_trans.header.stamp = state.current_time;
         n.getParam("uav_id", odom_trans.header.frame_id);
         n.getParam("child_frame_id", odom_trans.child_frame_id);
-        odom_trans.transform.translation.x = x;
-		odom_trans.transform.translation.y = y;
+        odom_trans.transform.translation.x = state.x;
+		odom_trans.transform.translation.y = state.y;
 		odom_trans.transform.translation.z = 0.0;
 		odom_trans.transform.rotation.x = odom_quat.x();
 		odom_trans.transform.rotation.y = odom_quat.y();
@@ -68,12 +75,12 @@ int main(int argc, char **argv){
 
         // Publish the odometry message over ROS
 		nav_msgs::Odometry odom;
-		odom.header.stamp = current_time;
+		odom.header.stamp = state.current_time;
         n.getParam("uav_id", odom.header.frame_id);
 
         // Set the position
-		odom.pose.pose.position.x = x;
-		odom.pose.pose.position.y = y;
+		odom.pose.pose.position.x = state.x;
+		odom.pose.pose.position.y = state.y;
 		odom.pose.pose.position.z = 0.0;
         odom.pose.pose.orientation.x = odom_quat.x();
 		odom.pose.pose.orientation.y = odom_quat.y();
@@ -82,9 +89,9 @@ int main(int argc, char **argv){
 
 		// Set the velocity
 		n.getParam("child_frame_id", odom.child_frame_id);
-		odom.twist.twist.linear.x = vx;
-		odom.twist.twist.linear.y = vy;
-		odom.twist.twist.angular.z = vth;
+		odom.twist.twist.linear.x = state.vx;
+		odom.twist.twist.linear.y = state.vy;
+		odom.twist.twist.angular.z = state.v_yaw;
 
 		// Publish the message
 		odom_pub.publish(odom);
@@ -94,7 +101,21 @@ int main(int argc, char **argv){
         ROS_INFO_STREAM("Coords: [" << odom.pose.pose.position.x << ", " << odom.pose.pose.position.y << ", " << odom.pose.pose.position.z << "]");
         ROS_INFO_STREAM("Vel: [" << odom.twist.twist.linear.x << ", " << odom.twist.twist.linear.y << ", " << odom.twist.twist.angular.z << "]");
 
-        ros::spinOnce();
+		ros::spinOnce();
         loop_rate.sleep();
     }
+}
+
+// Update the 6-DOF state for each time increment
+// Currently we assume 2D circular motion. @TODO: To expand to 3D motion
+void compute_odom(six_dof *state){
+	state->current_time = ros::Time::now();
+	double dt = (state->current_time - state->last_time).toSec();
+	double delta_x = (state->vx * cos(state->yaw) - state->vy * sin(state->yaw)) * dt;
+	double delta_y = (state->vx * sin(state->yaw) + state->vy * cos(state->yaw)) * dt;
+	double delta_yaw = state->v_yaw * dt;
+	state->x += delta_x;
+	state->y += delta_y;
+	state->yaw += delta_yaw;
+	state->last_time = state->current_time;
 }
